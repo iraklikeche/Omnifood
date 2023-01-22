a123f1b4e6014d953d6c47bc6a0ea9276d780e02
iraklikeche <106651425+iraklikeche@users.noreply.github.com>
2023-01-22 20:59:46 +0400

    Add files via upload

---
+++ Omnifood/script.js
@@ -0,0 +1,54 @@
+const yearEl = document.querySelector(".year");
+
+yearEl.textContent = new Date().getFullYear();
+
+const btnEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const links = document.querySelectorAll("a:link");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
